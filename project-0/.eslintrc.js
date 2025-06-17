59a1cafae4f60cb763120228398b3b16273110c6
A1iHassan <ali012wkout@gmail.com>
2025-06-17 17:17:11 +0300

    added lingin rule to the project

---
+++ project-0/.eslintrc.js
@@ -0,0 +1,57 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:node/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+
+    "node/no-unsupported-features/es-syntax": "off",
+    "node/no-missing-import": "off",
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+};
