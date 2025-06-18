d1c63495597788ad821c089867ab242a2cb49111
A1iHassan <ali012wkout@gmail.com>
2025-06-18 13:53:01 +0300

    update lint rules

--- project-0/eslint.config.js
+++ project-0/eslint.config.js
@@ -1,49 +1,59 @@
-/** @type {import("eslint").Linter.Config} */
-export const root = true;
-export const ignores = ["node_modules/", "dist/", "build/", "bun.lockb"];
-export const parser = "@typescript-eslint/parser";
-export const parserOptions = {
-  ecmaVersion: 2020,
-  sourceType: "module",
-  project: ["./tsconfig.json"],
-};
-export const env = {
-  node: true,
-  es2021: true,
-};
-export const Extends = [
-  "eslint:recommended",
-  "plugin:@typescript-eslint/recommended",
-  "plugin:import/recommended",
-  "plugin:import/typescript",
-  "plugin:node/recommended",
-  "prettier",
-];
-export const plugins = ["@typescript-eslint", "import"];
-export const rules = {
-  "no-console": "warn",
-  "no-unused-vars": "off",
-  "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+// eslint.config.js
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginNode from "eslint-plugin-node";
+import eslintPluginTS from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-  "import/order": [
-    "warn",
-    {
-      groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-      "newlines-between": "always",
-    },
-  ],
-
-  "@typescript-eslint/explicit-function-return-type": "off",
-  "@typescript-eslint/no-explicit-any": "warn",
-
-  "node/no-unsupported-features/es-syntax": "off",
-  "node/no-missing-import": "off",
-};
-export const overrides = [
+export default [
   {
-    files: ["*.ts"],
-    parserOptions: {
-      project: ["./tsconfig.json"],
+    files: ["**/*.ts"],
+    ignores: ["node_modules/", "dist/", "build/", "bun.lockb"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        module: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": eslintPluginTS,
+      import: eslintPluginImport,
+      node: eslintPluginNode,
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      "node/no-unsupported-features/es-syntax": "off",
+      "node/no-missing-import": "off",
     },
   },
 ];
